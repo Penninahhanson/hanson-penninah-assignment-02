5b8ae0984f469d682d90e98689157997b79cc5aa
penninah hanson <109240461+penninah200@users.noreply.github.com>
2022-07-13 18:52:20 +0000

    worked on challenge-1 and challenge-2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   sum = 0;
+  arrayOfNumbers.forEach((item)=> sum+=item)
+  return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    evenNum = 0;
+  arrayOfNumbers.forEach((number)=>{
+   if(number % 2 == 0){evenNum += 1
+  }else{
+    evenNum += 0
+  }})
+  return evenNum
+     
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
